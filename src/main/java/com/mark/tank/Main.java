1a807aabb789a148535a5897c767c64c656684b2
makang <makang@cdvcloud.com>
2019-11-19 21:27:03 +0800

    更新-tank动起来-马康-2019年11月19日21:26:54

---
+++ src/main/java/com/mark/tank/Main.java
@@ -0,0 +1,25 @@
+package com.mark.tank;
+
+/**
+ * Created by makang on 2019/11/19.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        Frame f = new Frame();
+//        f.setSize(800,600);
+//        f.setResizable(false);
+//        f.setTitle("tank war");
+//        f.setVisible(true);
+//
+//        f.addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent e) {
+//                System.exit(0);
+////                super.windowClosing(e);
+//            }
+//        });
+
+        TankFrame tankFrame = new TankFrame();
+    }
+}
---
+++ src/main/java/com/mark/tank/TankFrame.java
@@ -0,0 +1,35 @@
+package com.mark.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by makang on 2019/11/19.
+ */
+public class TankFrame extends Frame {
+
+    int x =200, y = 200;
+
+   public TankFrame(){
+       setSize(800,600);
+       setResizable(false);
+       setTitle("tank war");
+       setVisible(true);
+
+       addWindowListener(new WindowAdapter() {
+           @Override
+           public void windowClosing(WindowEvent e) {
+               System.exit(0);
+//               super.windowClosing(e);
+           }
+       });
+   }
+
+    @Override
+    public void print(Graphics g) {
+        g.fillRect(x,y,60,60);
+        x += 10;
+        y += 10;
+    }
+}
