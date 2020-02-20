2e82a9a295b3eeb31351d4b422db84962ae3f6cb
makang <makang@cdvcloud.com>
2020-02-20 21:14:12 +0800

    更新-定义子弹类设定属性和方法，new出一个子弹类，在屏幕上画出来-马康-2020年2月20日21:14:02

---
+++ src/main/java/com/mark/tank/Bullet.java
@@ -0,0 +1,46 @@
+package com.mark.tank;
+
+import java.awt.*;
+
+/**
+ * Created by makang on 2020/2/20.
+ */
+public class Bullet {
+    private static final int SPEED = 10;
+    private static final int WIDTH = 30,HEIGHT = 30;
+
+    private int x,y;
+    private Dir dir;
+
+    public Bullet(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x,y,WIDTH,HEIGHT);
+        g.setColor(c);
+        move();
+    }
+
+    private void move(){
+        switch (dir){
+            case LEFT:
+                x-=SPEED;
+                break;
+            case UP:
+                y-=SPEED;
+                break;
+            case RIGHT:
+                x+=SPEED;
+                break;
+            case DOWN:
+                y+=SPEED;
+                break;
+        }
+    }
+
+}
