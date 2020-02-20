02a1b196fceeb228b7d9724a9b684c73a0b8dd3c
makang <makang@cdvcloud.com>
2020-02-20 22:27:36 +0800

    更新-单元测试方法-马康-=2020年2月20日22:27:25

---
+++ src/main/java/test/ImageTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Created by makang on 2020/2/20.
+ */
+class ImageTest {
+
+    public void test1(){
+
+    }
+
+    @Test
+    void test(){
+
+//        fail("");
+        assertNotNull(new Object());
+
+    }
+}
