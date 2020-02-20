abc04f180bfedfd667d6a0d7b0fd084463879a85
makang <makang@cdvcloud.com>
2020-02-20 20:58:10 +0800

    更新-抽象出tank类，封装print方法进行调用-马康-2020年2月20日20:58:02

--- src/main/java/com/mark/tank/TankFrame.java
+++ src/main/java/com/mark/tank/TankFrame.java
@@ -11,9 +11,7 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
 
-    int x =20, y = 20;
-    Dir dir = Dir.DOWN;
-    private static final int SPEED = 10;
+    Tank myTank = new Tank(200,200,Dir.DOWN);
 
    public TankFrame(){
        setSize(800,600);
@@ -34,21 +32,7 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(x,y,60,60);
-        switch (dir){
-            case LEFT:
-                x-=SPEED;
-                break;
-            case UP:
-                y-=SPEED;
-                break;
-            case RIGHT:
-                x+=SPEED;
-                break;
-            case DOWN:
-                y+=SPEED;
-                break;
-        }
+        myTank.paint(g);
     }
 
     class MyKeyListener extends KeyAdapter {
@@ -100,10 +84,10 @@ public class TankFrame extends Frame {
             setMainTainkDir();
         }
         private void setMainTainkDir() {
-            if(bL){dir = Dir.LEFT;};
-            if(bU){dir = Dir.UP;};
-            if(bR){dir = Dir.RIGHT;};
-            if(bD){dir = Dir.DOWN;};
+            if(bL){myTank.setDir(Dir.LEFT);};
+            if(bU){myTank.setDir(Dir.UP);};
+            if(bR){myTank.setDir(Dir.RIGHT);};
+            if(bD){myTank.setDir(Dir.DOWN);};
         }
     }
 
