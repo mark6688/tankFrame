54debb731f58e4bac0e8b30041fd451a31d03bee
makang <mk740474106@163.com>
2020-02-24 23:55:38 +0800

    更新-lamada表达式的一部分理解-马康-2020年2月24日23:54:54

--- src/main/java/test/ImageTest.java
+++ src/main/java/test/ImageTest.java
@@ -14,13 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  */
 public class ImageTest {
 
-    @Test
+//    @Test
     void test()throws Exception{
 
 //        BufferedImage image = ImageIO.read(new File("C:/idea/1.jpg"));
 //        assertNotNull(image);
         BufferedImage images = ImageIO.read(ImageTest.class.getClassLoader().getResourceAsStream("images/1.gif"));
         assertNotNull(images);
+
+    }
+
+    public static void main(String[] args) {
+        //lamada表达式，将类的实例化省略，类中只有一个方法的话，也将方法省略而用->代替的作用
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("线程创建");
+            }
+        }).start();
+
+        new Thread(()->{
+            System.out.println("lamada表达式创建");
+        }).start();
 
     }
 }
