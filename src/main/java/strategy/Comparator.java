7675561dec8b51ba056bcf5f34d0dfef84b1c474
makang <mk740474106@163.com>
2020-02-26 00:57:18 +0800

    更新-函数式接口，java8内容扩充-马康-2020年2月26日00:56:30

--- src/main/java/strategy/Comparator.java
+++ src/main/java/strategy/Comparator.java
@@ -1,5 +1,16 @@
 package strategy;
 
+//如果一个接口中只有一个方法，则称之为函数式方法
+//或者也可以加如下注解表示该接口为函数接口
+//@FunctionalInterface
 public interface Comparator<T> {
     int compareTo(T o1,T o2);
+
+    //jdk1.8之后才支持的接口中写默认方法
+    default void m(){
+        System.out.println("m");
+    }
+    default void d(){
+        System.out.println("m");
+    }
 }
--- src/main/java/strategy/Main.java
+++ src/main/java/strategy/Main.java
@@ -10,12 +10,18 @@ public class Main {
         Dog[] d = {new Dog(1),new Dog(8),new Dog(3)};
         Sorter<Dog> sorter = new Sorter();
         sorter.sort(d,new DogComparator());
+        System.out.println(Arrays.toString(d));
 
         Sorter<Cat> sorter1 = new Sorter();
-        sorter1.sort(a,new CatWeightComparator());
-
-        System.out.println(Arrays.toString(d));
+//        sorter1.sort(a,new CatWeightComparator());
+        //函数式接口：
+        sorter1.sort(a,(o1,o2)->{
+            if(o1.weight < o2.weight){return -1;}
+            else if(o1.weight > o1.weight){return 1;}
+            else return 0;
+        });
         System.out.println(Arrays.toString(a));
+
     }
 
 }
